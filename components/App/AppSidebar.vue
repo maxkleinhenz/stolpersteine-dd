3cda634256d9b93639fb87c1b6dd43c4f147cb5a
maxkleinhenz <max.github@outlook.de>
2023-04-10 20:40:20 +0200

    Teleport

--- components/App/AppSidebar.vue
+++ components/App/AppSidebar.vue
@@ -10,31 +10,34 @@ const isOpen = useVModel(props, "open", emit);
 
 <template>
   <div class="flex min-h-screen flex-row">
-    <ClientOnly>
-      <div
-        class="absolute left-0 z-30 min-h-screen transition-transform duration-300 lg:relative"
-        :class="{ '-translate-x-full': !isOpen, absolute: overlay }"
-      >
-        <slot name="sidebar" />
-      </div>
-    </ClientOnly>
-
     <div class="flex-1">
       <slot name="main" />
-      <Transition
-        enter-active-class="duration-300 ease-out"
-        enter-from-class="transform opacity-0"
-        enter-to-class="opacity-100"
-        leave-active-class="duration-200 ease-in"
-        leave-from-class="opacity-100"
-        leave-to-class="transform opacity-0"
-      >
-        <div
-          v-show="overlay && open"
-          class="absolute inset-0 z-20 cursor-pointer bg-gray-700/30"
-          @click="() => (isOpen = false)"
-        ></div>
-      </Transition>
     </div>
+
+    <ClientOnly>
+      <Teleport to="body" :disabled="!overlay">
+        <Transition
+          enter-active-class="duration-300 ease-out"
+          enter-from-class="transform opacity-0"
+          enter-to-class="opacity-100"
+          leave-active-class="duration-200 ease-in"
+          leave-from-class="opacity-100"
+          leave-to-class="transform opacity-0"
+        >
+          <div
+            v-if="overlay && open"
+            class="absolute inset-0 cursor-pointer bg-gray-700/30"
+            @click="() => (isOpen = false)"
+          ></div>
+        </Transition>
+
+        <div
+          class="left-0 top-0 z-20 min-h-screen transition-transform duration-300"
+          :class="{ '-translate-x-full': !isOpen, absolute: overlay, relative: !overlay }"
+        >
+          <slot name="sidebar" />
+        </div>
+      </Teleport>
+    </ClientOnly>
   </div>
 </template>
