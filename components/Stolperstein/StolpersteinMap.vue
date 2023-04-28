4e64f80c99f8108180fd32c9fe0888a22abfda47
maxkleinhenz <max.github@outlook.de>
2023-04-28 22:14:03 +0200

    refactor stolperstein slider

--- components/Stolperstein/StolpersteinMap.vue
+++ components/Stolperstein/StolpersteinMap.vue
@@ -7,7 +7,7 @@
       /></a>
     </div>
 
-    <StolpersteinSlider class="absolute inset-x-0 bottom-0 z-10 overflow-hidden" />
+    <StolpersteinSlider class="absolute inset-x-0 bottom-0 overflow-hidden" />
 
     <!-- <div class="map-controls column absolute-bottom-right"></div> -->
   </div>
