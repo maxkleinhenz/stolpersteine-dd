c0e636d938ebc4b9489025dc974bbd35365f5588
maxkleinhenz <max.github@outlook.de>
2023-06-17 21:09:32 +0200

    refactor navigation

--- components/App/AppNavigation.vue
+++ components/App/AppNavigation.vue
@@ -4,19 +4,16 @@ const links = {
   karte: { name: "Karte", to: "/karte" },
   info: { name: "Info", to: "/info" },
 };
-
-const { isMinTablet } = useAppBreakpoints();
 </script>
 
 <template>
   <nav
-    class="fixed inset-x-0 bottom-0 z-20 flex rounded-t-2xl bg-white text-lg shadow-top md:bottom-auto md:left-auto md:top-0 md:m-8 md:rounded-full md:px-9 md:shadow-md"
+    class="fixed inset-x-0 bottom-0 z-20 flex rounded-t-2xl bg-white text-lg shadow-top md:bottom-auto md:left-auto md:top-0 md:m-8 md:rounded-full md:px-6 md:shadow-md"
   >
     <AppButton
       v-for="link in links"
-      class="flex-1 border-b-4 border-transparent py-5 text-center hover:border-accent-2"
+      class="flex-1 border-b-4 border-transparent px-0 py-5 text-center hover:border-accent-2 md:px-5"
       intent="default"
-      :size="isMinTablet ? 'large' : 'small'"
       :to="link.to"
       >{{ link.name }}</AppButton
     >
