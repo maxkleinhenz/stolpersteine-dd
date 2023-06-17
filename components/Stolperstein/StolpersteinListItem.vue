80f9a11a3c6bf92771d946d94b37557363b6c0b7
maxkleinhenz <max.github@outlook.de>
2023-06-17 21:15:50 +0200

    refactor stolperstein item

--- components/Stolperstein/StolpersteinListItem.vue
+++ components/Stolperstein/StolpersteinListItem.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <AppButton
-      class="flex gap-3 rounded-2xl bg-base-2 p-3 ring-accent-2 transition-shadow hover:shadow-lg hover:ring-1"
+      class="flex gap-2 rounded-2xl bg-base-2 ring-accent-2 transition-shadow hover:shadow-lg hover:ring-1 md:gap-3"
       :to="`/karte/${stolperstein.stolperstein.id}`"
       :replace="replace"
       intent="secondary"
@@ -13,9 +13,11 @@
         />
       </div>
       <div class="flex-1">
-        <p class="text-lg font-semibold">{{ stolperstein.stolperstein.name }}</p>
-        <p class="">{{ stolperstein.stolperstein.strasseHausnummer }}</p>
-        <p class="">{{ stolperstein.stolperstein.plzOrt }}</p>
+        <p class="pb-1 text-lg font-semibold leading-6">{{ stolperstein.stolperstein.name }}</p>
+        <p class="leading-5">
+          {{ stolperstein.stolperstein.strasseHausnummer }}<br />
+          {{ stolperstein.stolperstein.plzOrt }}
+        </p>
       </div>
       <div class="flex-initial place-self-center"><AppIcon name="ic:baseline-chevron-right" size="small" /></div>
     </AppButton>
