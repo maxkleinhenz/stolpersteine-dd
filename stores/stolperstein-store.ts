ab0e73d09161e35e86318ccb3b4a6a30c0a7b1e8
maxkleinhenz <max.github@outlook.de>
2023-06-25 22:14:16 +0200

    clean up

--- stores/stolperstein-store.ts
+++ stores/stolperstein-store.ts
@@ -1,5 +1,5 @@
 import { defineStore } from "pinia";
-import { StolpersteinFeature, StolpersteinResult } from "~~/models/stolperstein.model";
+import { StolpersteinFeature } from "~~/models/stolperstein.model";
 
 export const useStolpersteinStore = defineStore("stolperstein", () => {
   const stolpersteine = ref<StolpersteinFeature[]>([]);
@@ -23,42 +23,10 @@ export const useStolpersteinStore = defineStore("stolperstein", () => {
     });
   });
 
-  async function fetchStolpersteine() {
-    const data = await $fetch<StolpersteinResult>("/stolpersteine/stolpersteine.json");
-    const features = data.features;
-    const stolpersteinFeatures = features.map((feature) => {
-      return {
-        stolperstein: {
-          id: feature.properties.id,
-          name: feature.properties.einrichtung,
-          firstName: feature.properties.einrichtung.split(", ")[1],
-          lastName: feature.properties.einrichtung.split(", ")[0],
-          strasse: feature.properties.strasse,
-          hausnummer: feature.properties.hnr,
-          hausnummerZusatz: feature.properties.hnrz,
-          strasseHausnummer:
-            [feature.properties.strasse, feature.properties.hnr].join(" ") + (feature.properties.hnrz ?? ""),
-          plz: feature.properties.plz,
-          ort: "Dresden",
-          plzOrt: [feature.properties.plz, "Dresden"].join(" "),
-          url: feature.properties.url,
-          stolpersteinImage: "/stolpersteine/images/default.jpg",
-          stolpersteinThumbImage: "/stolpersteine/images/thumb.jpg",
-          sortValue: feature.properties.einrichtung.toLowerCase(),
-        },
-        geometry: feature.geometry,
-      } as StolpersteinFeature;
-    });
-    stolpersteine.value = stolpersteinFeatures.sort((a, b) =>
-      a.stolperstein.sortValue < b.stolperstein.sortValue ? -1 : 1
-    );
-  }
-
   return {
     stolpersteine,
     searchText,
     selectedStolpersteine,
     filteredStolpersteine,
-    fetchStolpersteine,
   };
 });
